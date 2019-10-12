221f0bf4c1c9c812283c4dfd811c543497fda29b
Aleix <aleixmp@outlook.com>
2019-10-12 13:56:18 +0300

    Page detail component.
    Component to show  album/artist details.

--- SpotiViewer/src/app/components/page-detail/page-detail.component.ts
+++ SpotiViewer/src/app/components/page-detail/page-detail.component.ts
@@ -1,4 +1,17 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {AlbumDetail} from '../../interfaces/album-detail';
+import {ArtistDetail} from '../../interfaces/artist-detail';
+
+interface PageDetailItem {
+  label: string;
+  value: string | string[];
+}
+
+interface PageDetailData {
+  name: string;
+  image: string;
+  details: PageDetailItem [];
+}
 
 @Component({
   selector: 'app-page-detail',
@@ -6,11 +19,36 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./page-detail.component.scss']
 })
 export class PageDetailComponent implements OnInit {
+  @Input() data: AlbumDetail | ArtistDetail;
+
+  detail: PageDetailData;
 
   constructor() {
   }
 
   ngOnInit() {
+    this.detail = {
+      name: this.data.name,
+      image: this.data.image,
+      details: [
+        {
+          label: 'Release date',
+          value: this.data.type
+        },
+        {
+          label: 'Genres',
+          value: this.data.genres
+        }
+      ]
+    };
+
+    if ('release_date' in this.data) {
+      this.detail.details.push({
+        label: 'Type',
+        value: this.data.release_date
+      });
+    }
+
   }
 
 }
--- SpotiViewer/src/app/pipes/genres-format.pipe.ts
+++ SpotiViewer/src/app/pipes/genres-format.pipe.ts
@@ -6,14 +6,13 @@ import {Pipe, PipeTransform} from '@angular/core';
 export class FormatDetailsPipe implements PipeTransform {
 
   transform(genres: string[]): any {
-    if (genres && genres.length > 0) {
+    if (genres && Array.isArray(genres) && genres.length > 0) {
       return genres.join(', ');
-    } else if (genres && genres.length === 0) {
+    } else if (genres && Array.isArray(genres) && genres.length === 0) {
       return 'There is no genres';
     }
 
     return genres;
-
   }
 
 }
